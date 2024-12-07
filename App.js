9a5c9e77122191926ff955324cdcb0a72e3c89d9
ROHIT RAJ <rj14771@gmail.com>
2024-12-07 08:46:19 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text, Platform, KeyboardAvoidingView, SafeAreaView, ScrollView } from "react-native";
+import {actions, RichEditor, RichToolbar} from "react-native-pell-rich-editor";
+
+
+const handleHead = ({tintColor}) => <Text style={{color: tintColor}}>H1</Text>
+const headinghaed2 = ({tintColor}) => <Text style={{color: tintColor}}>H2</Text>
+
+const App = () => {
+	const richText = React.useRef();
+	return (
+    <SafeAreaView>
+      <ScrollView>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}	style={{ flex: 1 }}>
+          <Text>Description:</Text>
+          <RichEditor
+              ref={richText}
+              onChange={ descriptionText => {
+                  console.log("descriptionText:", descriptionText);
+              }}
+          />
+        </KeyboardAvoidingView>
+      </ScrollView>
+
+      <RichToolbar
+        editor={richText}
+        actions={[ actions.setBold, actions.setItalic, actions.setUnderline, actions.heading1, actions.heading2 ]}
+        iconMap={{ [actions.heading1]: handleHead, [actions.heading2]:headinghaed2 }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
